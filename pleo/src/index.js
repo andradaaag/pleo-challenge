45565ffb5a79dcb3459c4b594cf649d1af2cd4d8
Andrada Gae <gaeandrada@gmail.com>
2020-02-07 14:14:48 +0100

    Persist data into local store

--- pleo/src/index.js
+++ pleo/src/index.js
@@ -2,20 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import thunk from 'redux-thunk';
 import App from './App';
 import './index.css';
 import reducers from './reducers';
 
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducers = persistReducer(persistConfig, reducers)
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    reducers,
+    persistedReducers,
     composeEnhancer(applyMiddleware(thunk)),
 );
+const persistor = persistStore(store)
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root'));
