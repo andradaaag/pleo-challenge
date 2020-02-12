f2b929df956c1d0634fdb44d435627edf9ff09d8
Andrada Gae <gaeandrada@gmail.com>
2020-02-12 16:08:35 +0100

    Add simple filter functionality based on merchant

--- pleo/src/components/Expenses.js
+++ pleo/src/components/Expenses.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import ExpenseItem from './ExpenseItem';
 import { connect } from 'react-redux';
 import * as expensesActions from '../actions';
+import Filter from './Filter'
 
 class Expenses extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filter: ''
+        }
+        this.handleFilter = this.handleFilter.bind(this)
+    }
     componentDidMount() {
         this.props.getExpenses()
+    }
+
+    handleFilter(event) {
+        const filter = event.target.value
+        this.setState({
+            filter: filter
+        })
     }
 
     render() {
@@ -13,9 +28,20 @@ class Expenses extends Component {
             return <div />
         }
         return (
-            this.props.expenses.map((expense) => (
-                <ExpenseItem expense={expense} key={expense.id} />
-            ))
+            <div>
+                <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
+                <div>
+                    {this.props.expenses
+                        .filter((expense) => {
+                            const merchant = expense.merchant.toLowerCase()
+                            const filter = this.state.filter.toLowerCase()
+                            return merchant.includes(filter)
+                        })
+                        .map((expense) => {
+                            return (<ExpenseItem expense={expense} key={expense.id} />)
+                        })}
+                </div>
+            </div>
         )
     }
 }
---
+++ pleo/src/components/Filter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Filter extends Component {
+    render() {
+        return (
+            <div>
+                <label>
+                    Filter
+        <input type="text" name="filter"
+                        value={this.props.filter}
+                        onChange={this.props.handleFilter}
+                    />
+                </label>
+            </div>
+        );
+    }
+}
+export default Filter
