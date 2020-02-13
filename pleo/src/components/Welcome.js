9c924e586dff55f86c6118e79ba55fe0cad31a4a
Andrada Gae <gaeandrada@gmail.com>
2020-02-13 14:50:10 +0100

    Small style improvements to Welcome page

--- pleo/src/components/Welcome.js
+++ pleo/src/components/Welcome.js
@@ -33,7 +33,7 @@ class Welcome extends Component {
                 <img src={logo} id="logo" alt="Cool Logo" ahref="https://www.pleo.io/"/>
                 <br/>
                 {this.renderRedirect()}
-                <input type="text" id="userEmail" value={this.state.userEmail}
+                <input type="text" id="userEmail" value={this.state.userEmail} spellCheck="false"
                     onChange={this.handleEmail.bind(this)} placeholder="You email here"
                 />
                 <br/>
