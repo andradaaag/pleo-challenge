55eb9bcb18457e19f996d3b529af697dd26c59cd
Andrada Gae <gaeandrada@gmail.com>
2020-02-13 22:04:16 +0100

    Style end message

---
+++ pleo/src/components/EndMessage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EndMessage extends Component {
+    render() {
+        return (
+            <div>
+                <label id="endMessage">That's all folks!</label>
+            </div>
+        );
+    }
+}
+
+export default EndMessage
